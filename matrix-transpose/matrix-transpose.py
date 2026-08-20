4a51a9047e05e49ace8c72eafb51c38d5e5f5c26
Jaisukh Reddy <jaisukhreddy123@gmail.com>
2026-08-21 00:46:39 +0530

    Solve matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -4,14 +4,7 @@ def matrix_transpose(A: list) -> np.ndarray:
     """
     Return the transpose of matrix A (swap rows and columns).
     """
-    A = np.asarray(A)
-    rows,col = A.shape
-    result = np.empty((col,rows),dtype=A.dtype)
-
-    for i in range(rows):
-        for j in range(col):
-            result[j,i]=A[i,j]
-
-    return result
+    return np.asarray(A).T
+    
             
      
